31de6dde8cbe654a6c84db3fed50201c8f675ea8
Brett Tanner <bretttanner171@gmail.com>
2022-08-13 23:00:13 +0900

    Define #knight_offsets & #print_board

--- board.rb
+++ board.rb
@@ -1,16 +1,39 @@
 class Board
+    attr_accessor :board
+
     def initialize
-        @board = create_board()
+        @board = Array.new(8) {Array.new(8, 0)}
+        @knight_offsets = knight_offsets()
+        @adjacency_list = create_list()
     end
 
-    def create_board
-        board = []
-        8.times {board << Array.new(8, 0)} 
-        board
+    def knight_offsets
+        row_moves = [-1, 1]
+        col_moves = [-2, 2]
+        valid_moves = row_moves.product(col_moves).concat(col_moves.product(row_moves))
     end
-    
+
+    def create_list
+        list = Hash.new
+        valid_coord = Array.new(8) {|i| i}
+        valid_coord.repeated_permutation(2) {|permutation| list[permutation] = nil}
+        print_board()
+        list.each {|k, v| list[k] = valid_moves(k)}
+    end
+
     def knight_moves(start, final)
         
+    end
+
+    def valid_moves(start)
+        # valid_ends = []
+        # @knight_offsets.map(start) {|row, column| }
+        
+        # @board.fetch(row + 1, []).fetch(col + 2, nil)
+    end
+
+    def print_board
+        @board.each {|row| puts row.join(" ")}
     end
 end
 
