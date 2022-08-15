e69a68d44cf421b266d53a77ed09586d9718deeb
Brett Tanner <bretttanner171@gmail.com>
2022-08-15 16:37:22 +0900

    Fix @move_list being passed pointer to temp_list

--- board.rb
+++ board.rb
@@ -4,6 +4,7 @@ class Board
     def initialize
         @board = Array.new(8) {Array.new(8, 0)}
         @adjacency_list = create_list()
+        @adjacency_list.each {|k, v| puts "#{k}: #{v}"}
         # so first < check always passes
         @min_moves = 65
         @move_list = []
@@ -13,6 +14,16 @@ class Board
         row_moves = [-1, 1]
         col_moves = [-2, 2]
         valid_moves = row_moves.product(col_moves).concat(col_moves.product(row_moves))
+    end
+
+    def valid_moves(start, knight_offsets)
+        valid_ends = knight_offsets.map do |offset|
+            row = start[0] + offset[0]
+            col = start[1] + offset[1] 
+            next if row < 0 || row > 7 || col < 0 || col > 7
+            [row, col]
+        end
+        valid_ends.compact
     end
 
     def create_list
@@ -29,25 +40,18 @@ class Board
     end
 
     def search_list(start, fin, moves = 0, temp_list = [start])
-        if @adjacency_list[start].include?(fin) && moves < @min_moves
-            @min_moves = moves + 1
-            temp_list << fin
-            @move_list = temp_list
-            return
-        end
+        # FIXME: also, at some point it goes from 7,0 to 7,2 and 1,0 to 7,1. That shouldn't be possible
         @adjacency_list[start].each do |move|
-            knight_moves(move, fin, moves, temp_list)
+            next if temp_list.include?(move) || moves > @min_moves
+            moves += 1
+            temp_list << move
+            if move == fin && moves < @min_moves
+                @min_moves = moves.dup
+                @move_list = temp_list.dup
+                next
+            end
+            search_list(move, fin, moves, temp_list)
         end
-    end
-
-    def valid_moves(start, knight_offsets)
-        valid_ends = knight_offsets.map do |offset|
-            row = start[0] + offset[0]
-            col = start[1] + offset[1] 
-            next if row < 0 || row > 7 || col < 0 || col > 7
-            [row, col]
-        end
-        valid_ends.compact
     end
 
     def print_board
@@ -63,4 +67,4 @@ class Board
 end
 
 test = Board.new
-test.knight_moves([5, 4], [4, 2])
+test.knight_moves([0, 0], [2, 5])
